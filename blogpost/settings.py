d8e1005b97cdcd06a23113852d4867101c12d8d9
dansan-dsn <ddryn970@gmail.com>
2025-01-18 13:53:00 +0300

    media

--- blogpost/settings.py
+++ blogpost/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # authenticate user logins
 # AUTH_USER_MODEL = 'users.User'
 
--- blogpost/urls.py
+++ blogpost/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,6 +26,8 @@ urlpatterns = [
     path('comments/', include('comments.urls')),
     path('categories/', include('categories.urls')),
     path('reactions/', include('reactions.urls')),
-    # path('media/', include('media.urls')),
+    path('media/', include('media.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
